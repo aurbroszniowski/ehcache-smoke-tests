84e5cc09310ab9bd03fbddd3124ec7a5e729c489
Aurelien Broszniowski <aurelien@jsoft.biz>
2016-06-30 19:19:41 +0200

    added other ops

--- src/test/java/Ehcache3HeapOffheapTsa.java
+++ src/test/java/Ehcache3HeapOffheapTsa.java
@@ -1,14 +1,7 @@
 import io.rainfall.ObjectGenerator;
-import io.rainfall.Runner;
-import io.rainfall.Scenario;
 import io.rainfall.SyntaxException;
-import io.rainfall.configuration.ConcurrencyConfig;
-import io.rainfall.ehcache.statistics.EhcacheResult;
-import io.rainfall.ehcache3.Ehcache3Operations;
 import io.rainfall.generator.ByteArrayGenerator;
 import io.rainfall.generator.LongGenerator;
-import io.rainfall.generator.sequence.Distribution;
-import io.rainfall.statistics.StatisticsPeekHolder;
 import org.ehcache.Cache;
 import org.ehcache.PersistentCacheManager;
 import org.ehcache.clustered.client.config.ClusteredStoreConfiguration;
@@ -25,22 +18,10 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 
-import static io.rainfall.configuration.ReportingConfig.html;
-import static io.rainfall.configuration.ReportingConfig.report;
-import static io.rainfall.configuration.ReportingConfig.text;
-import static io.rainfall.ehcache.statistics.EhcacheResult.GET;
-import static io.rainfall.ehcache.statistics.EhcacheResult.MISS;
-import static io.rainfall.ehcache.statistics.EhcacheResult.PUT;
-import static io.rainfall.ehcache3.CacheConfig.cacheConfig;
-import static io.rainfall.ehcache3.Ehcache3Operations.get;
-import static io.rainfall.ehcache3.Ehcache3Operations.put;
-import static io.rainfall.ehcache3.Ehcache3Operations.remove;
-import static io.rainfall.execution.Executions.during;
-import static io.rainfall.unit.TimeDivision.minutes;
-import static java.util.concurrent.TimeUnit.DAYS;
 import static org.ehcache.config.builders.CacheManagerBuilder.newCacheManagerBuilder;
 
 
@@ -62,7 +43,7 @@ public class Ehcache3HeapOffheapTsa {
     int tsa = Integer.parseInt(System.getProperty("tsa"));
 
     long nbElementsHeap = MemoryUnit.MB.toBytes(heap) / MemoryUnit.KB.toBytes(1);
-    long nbElements = MemoryUnit.GB.toBytes(tsa) / MemoryUnit.KB.toBytes(1);
+    final long nbElements = MemoryUnit.GB.toBytes(tsa) / MemoryUnit.KB.toBytes(1);
 
     String tcHost = "localhost";
     CacheManagerBuilder<PersistentCacheManager> managerBuilder = newCacheManagerBuilder()
@@ -80,50 +61,46 @@ public class Ehcache3HeapOffheapTsa {
 
     String reportName = "tsa-" + heap;
 
-    ObjectGenerator<Long> keyGenerator = new LongGenerator();
-    ObjectGenerator<byte[]> valueGenerator = ByteArrayGenerator.fixedLength(1024);
+    final ObjectGenerator<Long> keyGenerator = new LongGenerator();
+    final ObjectGenerator<byte[]> valueGenerator = ByteArrayGenerator.fixedLength(1024);
 
     int nbThreads = Integer.parseInt(System.getProperty("nbThreads", "" + Runtime.getRuntime().availableProcessors()));
-    ConcurrencyConfig concurrency = ConcurrencyConfig.concurrencyConfig().threads(nbThreads).timeout(1, DAYS);
-
     String reportDir = System.getProperty("reportDir", ".");
 
     PersistentCacheManager cacheManager = null;
     try {
       cacheManager = managerBuilder.build(true);
 
-      Cache<Long, byte[]> cache = cacheManager.getCache("cache", Long.class, byte[].class);
+      final Cache<Long, byte[]> cache = cacheManager.getCache("cache", Long.class, byte[].class);
 
       Integer testLength = Integer.parseInt(System.getProperty("testLength", "7"));
 
       logger.info("----------> Test phase");
-      StatisticsPeekHolder finalStats = Runner.setUp(
-          Scenario.scenario("Test phase")
-              .exec(
-                  get(Long.class, byte[].class).using(keyGenerator, valueGenerator)
-                      .atRandom(Distribution.SLOW_GAUSSIAN, 0, nbElements, nbElements / 10)
-                      .withWeight(0.70),
-                  put(Long.class, byte[].class).using(keyGenerator, valueGenerator)
-                      .atRandom(Distribution.SLOW_GAUSSIAN, 0, nbElements, nbElements / 10)
-                      .withWeight(0.10),
-                  remove(Long.class, byte[].class).using(keyGenerator, valueGenerator)
-                      .atRandom(Distribution.SLOW_GAUSSIAN, 0, nbElements, nbElements / 10)
-                      .withWeight(0.10),
-                  Ehcache3Operations.replaceForKeyAndValue(Long.class, byte[].class).using(keyGenerator, valueGenerator)
-                      .atRandom(Distribution.SLOW_GAUSSIAN, 0, nbElements, nbElements / 10)
-                      .withWeight(0.10)
-              ))
-          .warmup(during(2, minutes))
-          .executed(during(testLength, minutes))
-          .config(concurrency)
-          .config(report(EhcacheResult.class)
-              .log(text(), html(reportDir + File.separatorChar + "test-" + reportName)))
-          .config(cacheConfig(Long.class, byte[].class)
-              .cache("cache", cache))
-          .start();
+      List<Thread> threads = new ArrayList<Thread>();
+      for (int j = 0; j < nbThreads; j++) {
+        threads.add(new Thread(new Runnable() {
+          public void run() {
+            for (long i = 0; i < nbElements; i++) {
+              cache.put(keyGenerator.generate(i), valueGenerator.generate(i));
+              cache.get(keyGenerator.generate(i));
+              cache.remove(keyGenerator.generate(i));
+              cache.putIfAbsent(keyGenerator.generate(i), valueGenerator.generate(i));
+              cache.replace(keyGenerator.generate(i), valueGenerator.generate(i));
+            }
+          }
+        }));
+      }
+
+      for (Thread thread : threads) {
+        thread.start();
+        try {
+          thread.join();
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+      }
+
       logger.info("----------> Done");
-    } catch (SyntaxException e) {
-      e.printStackTrace();
     } finally {
       if (cacheManager != null) {
         cacheManager.close();
